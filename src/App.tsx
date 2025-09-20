d7f5cd6d92473e5523e0f4e416b20c6c878faf27
sushanthkulal <sushanthkulal111@gmail.com>
2025-09-20 21:08:40 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -20,9 +20,11 @@ import { AddStaff } from './components/Hospital/AddStaff';
 import { AppointmentManagement } from './components/Hospital/AppointmentManagement';
 import { BillingManagement } from './components/Hospital/BillingManagement';
 import { ViewAllBilling } from './components/Hospital/ViewAllBilling';
+import { ScheduleAppointment } from './components/Hospital/ScheduleAppointment';
 import { PharmacyDashboard } from './components/Pharmacy/PharmacyDashboard';
 import { LabDashboard } from './components/Lab/LabDashboard';
 import { BillingPage } from './components/Billing/BillingPage';
+import { ReportImageViewer } from './components/Common/ReportImageViewer';
 
 // Protected Route Component
 const ProtectedRoute: React.FC<{ children: React.ReactNode; allowedRoles: string[] }> = ({ 
@@ -70,6 +72,14 @@ const AppRoutes: React.FC = () => {
         element={
           <ProtectedRoute allowedRoles={['doctor']}>
             <PatientDetail />
+          </ProtectedRoute>
+        } 
+      />
+      <Route 
+        path="/report/:reportId" 
+        element={
+          <ProtectedRoute allowedRoles={['doctor', 'patient']}>
+            <ReportImageViewer />
           </ProtectedRoute>
         } 
       />
@@ -178,6 +188,14 @@ const AppRoutes: React.FC = () => {
         element={
           <ProtectedRoute allowedRoles={['hospital']}>
             <ViewAllBilling />
+          </ProtectedRoute>
+        } 
+      />
+      <Route 
+        path="/hospital/schedule-appointment" 
+        element={
+          <ProtectedRoute allowedRoles={['hospital']}>
+            <ScheduleAppointment />
           </ProtectedRoute>
         } 
       />
